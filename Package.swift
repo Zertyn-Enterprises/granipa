ca7444a0a67941ce7ad06a47408d703d4473438d
zertyn <zertyn.llc@gmail.com>
2026-06-11 02:53:27 +0100

    feat: enable FluidAudio speaker diarization dependency

--- Package.swift
+++ Package.swift
@@ -4,18 +4,17 @@ import PackageDescription
 let package = Package(
     name: "Granipa",
     platforms: [.macOS("26.0")],
-    // To enable speaker diarization, add the FluidAudio dependency (exact pin:
-    // it has shipped breaking renames in minor releases) and its product below:
-    //   .package(url: "https://github.com/FluidInference/FluidAudio.git", exact: "0.15.2"),
-    //   .product(name: "FluidAudio", package: "FluidAudio"),
     dependencies: [
-        .package(url: "https://github.com/groue/GRDB.swift.git", from: "7.0.0")
+        .package(url: "https://github.com/groue/GRDB.swift.git", from: "7.0.0"),
+        // Exact pin: FluidAudio has shipped breaking renames in minor releases.
+        .package(url: "https://github.com/FluidInference/FluidAudio.git", exact: "0.15.2"),
     ],
     targets: [
         .executableTarget(
             name: "Granipa",
             dependencies: [
-                .product(name: "GRDB", package: "GRDB.swift")
+                .product(name: "GRDB", package: "GRDB.swift"),
+                .product(name: "FluidAudio", package: "FluidAudio"),
             ],
             path: "Sources/Granipa"
         ),
